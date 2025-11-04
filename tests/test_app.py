80c894f92dbaa8ff495e2c0ea210db2973f31b98
Naima <tarouktnaima@gmail.com>
2025-11-04 17:04:17 +0100

    Make test fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 from src.app import add, is_palindrome
 def test_add():
-    assert add(2,3) ==5
+    assert add(2,3) ==6
     assert add(-1, 1) ==0
 def test_is_palindrome():
     assert is_palindrome("radar")
